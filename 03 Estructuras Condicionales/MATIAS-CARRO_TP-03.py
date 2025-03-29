e1bd18dbe603d5d1d2b2af4819536518f493ba55
MatiasManuelCarro <matiasmanuelcarro@gmail.com>
2025-03-29 15:27:23 -0300

    Tp 3 ej 5

--- 03 Estructuras Condicionales/MATIAS-CARRO_TP-03.py
+++ 03 Estructuras Condicionales/MATIAS-CARRO_TP-03.py
@@ -20,3 +20,49 @@ if nota >= 6:
 else:
     print("Desaprobado")
 
+#Ejercicio 3
+#Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un
+#número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso
+#contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
+#operador de módulo (%) en Python para evaluar si un número es par o impar
+
+num = int(input("Ingrese un numero par:\n"))
+if num % 2 == 0:
+    print("Ha ingresado un número par")
+else:
+    print("Por favor, ingrese un número par")
+
+#Ejercicio 4
+#Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las
+#siguientes categorías pertenece:
+#● Niño/a: menor de 12 años.
+#● Adolescente: mayor o igual que 12 años y menor que 18 años.
+#● Adulto/a joven: mayor o igual que 18 años y menor que 30 años.
+#● Adulto/a: mayor o igual que 30 años.
+
+edad = int(input("Ingrese su edad:\n"))
+if edad >= 0 and edad <= 12:
+    print("Eres un Niño/a")
+elif edad >= 13 and edad < 18:
+    print("Eres un Adolescente")
+elif edad >= 18 and edad < 30:
+    print("Eres un Adulto/a joven")
+else:
+    print("Eres un Adulto/a")
+
+#Ejercicio 5
+#Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres
+#(incluyendo 8 y 14). Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en
+#pantalla el mensaje "Ha ingresado una contraseña correcta"; en caso contrario, imprimir por
+#pantalla "Por favor, ingrese una contraseña de entre 8 y 14 caracteres". Nota: investigue el uso
+#de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal
+#como una lista o un string.
+
+print("Ingrese una contraseña, la misma debe tener entre 8 y 14 caracteres")
+password = str(input("Ingrese la contraseña:\n"))
+
+if len(password) >= 8 and len(password) <= 14:
+    print("Ha ingresado una contraseña correcta")
+else:
+    print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
+
