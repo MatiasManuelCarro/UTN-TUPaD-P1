e698d95e7f2025258510fc0bda2e3bc494c30f35
MatiasManuelCarro <matiasmanuelcarro@gmail.com>
2025-03-29 16:51:20 -0300

    TP 3 Ej 9

--- 03 Estructuras Condicionales/MATIAS-CARRO_TP-03.py
+++ 03 Estructuras Condicionales/MATIAS-CARRO_TP-03.py
@@ -143,5 +143,34 @@ elif num == 2:
 elif num == 3:
     print(nombre.title()) #la funcion title cambia la primer letra de cada palabra del string a mayusculas
 
+#Ejercicio 9
+#Escribir un programa que pida al usuario la magnitud de un terremoto, clasifique la
+#magnitud en una de las siguientes categorías según la escala de Richter e imprima el resultado
+#por pantalla:
+#● Menor que 3: "Muy leve" (imperceptible).
+#● Mayor o igual que 3 y menor que 4: "Leve" (ligeramente perceptible).
+#● Mayor o igual que 4 y menor que 5: "Moderado" (sentido por personas, pero
+#generalmente no causa daños).
+#● Mayor o igual que 5 y menor que 6: "Fuerte" (puede causar daños en estructuras
+#débiles).
+#● Mayor o igual que 6 y menor que 7: "Muy Fuerte" (puede causar daños significativos).
+#● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala
 
+#solicitamos al usuario el ingreso de la magnitud
+escala = float(input("Ingrese la magnitud del terremoto para clasificarlo:\n"))
 
+#Condicional para la escala de ritcher
+if escala < 3:
+    print("Muy leve (imperceptible).")
+elif escala >= 3 and escala < 4:
+    print("Leve (ligeramente perceptible).")
+elif escala >= 4 and escala < 5:
+    print("Moderado (sentido por personas, pero generalmente no causa daños)")
+elif escala >= 5 and escala < 6:
+    print("Fuerte (puede causar daños en estructuras débiles)")
+elif escala >= 6 and escala < 7:
+    print("Muy Fuerte (puede causar daños significativos).")
+elif escala >= 7:
+    print("Extremo (puede causar graves daños a gran escala)")
+
+#
