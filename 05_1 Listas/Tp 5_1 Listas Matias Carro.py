ab460c69df59ea18860658d9725337a52835c870
Carro <matias.carro@sofse.gob.ar>
2025-05-06 12:00:38 -0300

    TP listas final

--- 05_1 Listas/Tp 5_1 Listas Matias Carro.py
+++ 05_1 Listas/Tp 5_1 Listas Matias Carro.py
@@ -17,19 +17,20 @@ print(lista_numero)
 #penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el
 #indexing con números negativos!
 
-print("Ejercicio 2")
+print(f"\nEjercicio 2")
 frutas = ["Manzana", "Banana", "Naranja", "Uva", "Pera"]
+print(f"Lista: {frutas}")
 
-#Utilizamosla funcion len para contar la cantidad de items en la lista 
-# #(se sabe que es 5 pero con len se aplica a cualquier lista ingresada)
-#Con el valor de len(frutas) le restamos -2 al indice de la lista. esto seria el anteultimo, ya que la lista va de 0 a 4 y el valor de len(frutas) es 5
+#Para mostrar el Penultimo elementos de la lista, utilizamos la posicion -2
+print(frutas[-2])
 
-print(frutas[len(frutas)-2])
+
+#Ejercicio 3
 
 #Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por
 #pantalla. Pista: para crear una lista vacía debes colocar los corchetes sin nada en su interior. Por
 
-print("Ejercicio 3")
+print(f"\nEjercicio 3")
 
 lista_ejercicio3 = []
 
@@ -45,36 +46,35 @@ print(lista_ejercicio3)
 #respectivamente. Imprimir la lista resultante por pantalla. ¡Puedes hacerlo como se muestra
 #en los videos o bien investigar cómo funciona el indexing con números negativos!
 
-print("Ejercicio 4")
+print(f"\nEjercicio 4")
 
 animales = ["perro", "gato", "conejo", "pez"]
+print(f"Lista inicial:\n {animales}")
 
 animales[1] = "loro" #reemplazamos el indice 1 de la lista, que es la segunda posicion
-animales[len(animales)-1] = "oso" #reemplazamos el valor -1 del largo total de la lista, que es la ultima posicion
+animales[-1] = "oso" #reemplazamos el valor -1 la lista, que es la ultima posicion
 
-print(animales)
+print(f"Lista final:\n {animales}")
 
 #Ejercicio 5
 #Analizar el siguiente programa y explicar con tus palabras qué es lo que realiza.
 
-print("Ejercicio 5")
+print(f"\nEjercicio 5")
 
 numeros =[8, 15, 3, 22, 7]
 numeros.remove(max(numeros))
 print(numeros)
 
 #Respuesta:
-# El programa remueve el valor maximo de la lista, utilizando la funcio max para encontrar el valor maximo
-#Y utiliza la funcion .remove para removerlo. Devuelve la lista sin el numero 22
-
-#TECNICATURA UNIVERSITARIA
+print("El programa remueve el valor maximo de la lista, utilizando la funcio max para encontrar el valor maximo \n")
+print("Y utiliza la funcion .remove para removerlo. Devuelve la lista sin el numero 22")
 
 #Ejercicio 6
 
 # Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por
 # pantalla los dos primeros.
 
-print("Ejercicio 6")
+print(f"\nEjercicio 6")
 
 lista_ejercicio6 = []
 
@@ -82,30 +82,35 @@ for i in range(10, 31, 5):
     lista_ejercicio6.append(i)
 
 print(lista_ejercicio6)
+print(f"Primeros dos numeros de la lista: {lista_ejercicio6[:2]}") #[:2] muestra desde el indice 0 al 1
 
 #Ejercicio 7
 
 #Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores
 #cualesquiera.
 
-print("Ejercicio 7")
+print(f"\nEjercicio 7")
 
 autos = ["sedan", "polo", "suran", "gol"]
+print(autos)
 
 autos[1] = "fiesta"
 autos[2] = "cronos"
+print("Lista con los valores centrales reemplazados:")
+print(autos)
 
 #Ejercicio 8
 
 #Crear una lista vacía llamada "dobles" y agregar el doble de 5, 10 y 15 usando append
 #directamente. Imprimir la lista resultante por pantalla
 
-print("Ejercicio 8")
+print(f"\nEjercicio 8")
 
 dobles = []
 
+#El ciclo for va a ir de 5 a 10 a 15
 for i in range(5, 16, 5):
-    dobles.append(i*2)
+    dobles.append(i*2) #agregamos el doble del valor de i
 
 print(dobles)
 
@@ -114,19 +119,27 @@ print(dobles)
 #Dada la lista “compras”, cuyos elementos representan los productos comprados por
 #diferentes clientes:
 
-print("Ejercicio 9")
+print(f"\nEjercicio 9")
 
 compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
-
+print("Lista inicial:")
+print(f"{compras}\n")
 #a-  Agregar "jugo" a la lista del tercer cliente usando append
+print("Agregar \"jugo\" a la lista del tercer cliente")
 compras[2].append("jugo") 
+print(f"{compras}\n")
 #b Reemplazar "fideos" por "tallarines" en la lista del segundo cliente.
+print("Reemplazar \"fideos\" por \"tallarines\" en la lista del segundo cliente.")
 compras[1][1] = "tallarines"
+print(f"{compras}\n")
 #c- Eliminar "pan" de la lista del primer cliente.
+print("Eliminar \"pan\" de la lista del primer cliente.")
 del compras[0][0]
+print(f"{compras}\n")
 
 #d- Imprimir la lista resultante por pantalla
-print(compras)
+print("Lista Final:\n")
+print(f"{compras}\n")
 
 #Ejercicio 10
 
@@ -140,7 +153,7 @@ print(compras)
 # Posición lista_anidada[3]: False
 #Imprimir la lista resultante por pantalla.
 
-print("Ejercicio 10")
+print(f"\nEjercicio 10")
 
 lista_anidada = [[], [], [], []]
 lista_anidada[0].append(15)
